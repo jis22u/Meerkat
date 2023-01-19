be7a0d77d500ff94b0ebab90b3c8117ab607f4b7
seunghwan <asdryzx@naver.com>
2023-01-19 17:09:13 +0900

    docs: 카카오맵 클러스터 구현 및 Figma 작업 이미지

---
+++ proposal/기획 구체화/카카오맵 클러스터/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Location from './Location.js'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Location/>
+    </div>
+  );
+}
+
+export default App;
