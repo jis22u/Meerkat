18fa56a93464fb6c636edd81d8ad8bf25150adb4
이동훈 <mankind1990s@gmail.com>
2023-02-05 05:32:20 +0900

    fix: be/ spring security 회원가입 인가 문제 수정 #20

--- server/src/main/java/B107/server/meerkat/config/security/auth/WebSecurityConfig.java
+++ server/src/main/java/B107/server/meerkat/config/security/auth/WebSecurityConfig.java
@@ -3,15 +3,28 @@ package B107.server.meerkat.config.security.auth;
 import B107.server.meerkat.config.security.filter.GlobalFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
     private final GlobalFilter globalFilter;
+
+    @Order(0)
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/sign");
+    }
 
     @Bean
     public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
@@ -22,13 +35,13 @@ public class WebSecurityConfig {
                 .addLogoutHandler(globalFilter.logoutHandler())
                 .logoutSuccessHandler(globalFilter.logoutSuccessHandler())
                 .and()
-                .addFilter(globalFilter.corsFilter())
-                .addFilter(globalFilter.authenticationFilter())
-                .addFilter(globalFilter.authorizationFilter())
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
                 .authorizeRequests()
-                .antMatchers(globalFilter.getPermitAll()).permitAll();
+                .antMatchers(globalFilter.getPermitAll()).permitAll()
+                .and()
+                .addFilter(globalFilter.corsFilter())
+                .addFilterBefore(globalFilter.authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(globalFilter.authorizationFilter(), BasicAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 }
