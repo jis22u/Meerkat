ba2df3883ad7e9eabb7fdab25ce4d40be3ce2ea8
NAM <nkj0901@naver.com>
2023-02-08 17:48:29 +0900

    style: footer 수정 및 디자인 작업

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,28 +1,65 @@
-import { Link } from 'react-router-dom';
-import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { useState } from 'react';
-import HomeIcon from '@mui/icons-material/Home';
-import ContactPageIcon from '@mui/icons-material/ContactPage';
-import LocalAtmIcon from '@mui/icons-material/LocalAtm';
+import { Link } from "react-router-dom";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import ContactPageIcon from "@mui/icons-material/ContactPage";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import HistoryIcon from "@mui/icons-material/History";
+import classes from "./Footer.module.css";
+import { createTheme } from "@mui/system";
 
 const Footer = () => {
-    const [selected , setSelected] = useState(0);
-    // 새로고침 할 때 Home으로 돌아가지는 것을 막아야함 (로컬 스토리지 이용하면 해결될 듯?)
-    return (
-        <footer>
-            <BottomNavigation
-              showLabels
-              value={selected}
-              onChange={(event, newValue) => {
-                setSelected(newValue);
-              }}
-            >
-              <BottomNavigationAction label="Home" icon={<HomeIcon />} component={Link} to="/" />
-              <BottomNavigationAction label="My Page" icon={<ContactPageIcon />} component={Link} to="/mypage" />
-              <BottomNavigationAction label="Cash" icon={<LocalAtmIcon />} component={Link} to="/cash"/>
-            </BottomNavigation>
-        </footer>
-    );
+
+  const meerkat = createTheme({
+    palette: {
+      background: {
+        default: "#F2E6C7"
+      }
+    }
+  });
+  
+  const [selected, setSelected] = useState(0);
+  // 새로고침 할 때 Home으로 돌아가지는 것을 막아야함 (로컬 스토리지 이용하면 해결될 듯?)
+  return (
+    <footer className={classes.footer}>
+      <BottomNavigation
+        sx={{ height: 80}}
+        theme={meerkat}
+        showLabels
+        value={selected}
+        onChange={(event, newValue) => {
+          setSelected(newValue);
+        }}
+      >
+        <BottomNavigationAction 
+          label="홈"
+          icon={<HomeIcon sx={{ color: "F2E6C7" }} />}
+          component={Link}
+          to="/"
+          id="icon"
+        />
+        <BottomNavigationAction
+        
+          label="마이페이지"
+          icon={<ContactPageIcon sx={{ width: 30}}/>}
+          component={Link}
+          to="/mypage"
+        />
+        <BottomNavigationAction
+          label="등록내역"
+          icon={<HistoryIcon />}
+          component={Link}
+          to="/registration-detail"
+        />
+        <BottomNavigationAction
+          label="코인"
+          icon={<LocalAtmIcon />}
+          component={Link}
+          to="/cash"
+        />
+      </BottomNavigation>
+    </footer>
+  );
 };
 
 export default Footer;
