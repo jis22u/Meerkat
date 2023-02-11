35b3823486ea3053c6c5616f996f950998ff1ade
이동훈 <mankind1990s@gmail.com>
2023-02-12 04:41:44 +0900

    chore: #28

--- client/src/pages/VideoChat.js
+++ client/src/pages/VideoChat.js
@@ -186,7 +186,7 @@ const VideoChat = () => {
       await getMedia();
       await makeConnection();
 
-      socketRef.current = io("http://i8b107.p.ssafy.io:8085", {
+      socketRef.current = io("http://43.201.72.34:8085", {
         query: `roomName=${roomName}`, //
       });
     
