70ff1a3a0860a4f6fbb0803bf529a3386d8a727f
jis22u <jis22u@gmail.com>
2023-02-12 18:06:51 +0900

    feat: coin 추가 #42

---
+++ server/src/main/java/B107/server/meerkat/controller/CoinController.java
@@ -0,0 +1,13 @@
+package B107.server.meerkat.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/coin")
+@RequiredArgsConstructor
+public class CoinController {
+}
