2ebd1b435ddf8c082773674c9a7dffc941bddfd1
jis22u <jis22u@gmail.com>
2023-02-12 22:44:59 +0900

    feat: 거래 구현 #42

--- server/src/main/java/B107/server/meerkat/repository/DealRepository.java
+++ server/src/main/java/B107/server/meerkat/repository/DealRepository.java
@@ -2,6 +2,11 @@ package B107.server.meerkat.repository;
 
 import B107.server.meerkat.entity.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DealRepository extends JpaRepository<Deal, Long> {
+
+	@Query("select d from Deal d where d.call.idx = :callIdx")
+	Deal findDealByCallIdx(@Param("callIdx") Long callIdx);
 }
