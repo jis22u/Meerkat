2e6c603c0c3316e4c92890ddbb679015e6079c75
dbsxowns2 <dbsxowns34@gmail.com>
2023-02-12 21:44:41 +0900

    Docs: Request Cash.js&Payment.js 사용자 코인 요청 및 업데이트

--- client/src/components/cash/Payment.js
+++ client/src/components/cash/Payment.js
@@ -3,17 +3,17 @@ import React from 'react';
 const  Payment = ({cash})=> {
   function onClickPayment() {
 
-    /* 1. 가맹점 식별하기 */
+    /* 가맹점 식별하기 */
     const { IMP } = window;
     IMP.init('imp21887438');
 
-    /* 2. 결제 데이터 정의하기 */
+    /* 결제 데이터 정의하기 */
     const data = {
         // pg : 'kakaopay.TC0ONETIME',
         pg : 'html5_inicis.INIpayTest',
         pay_method : 'card',  //생략가
         merchant_uid: "order_no_0001", //상점에서 생성한 고유 주문번호
-        name : '주문명:결제테스트',
+        name : '주문명: 코인 충전',
         amount : cash,
         buyer_email : 'test@portone.io',
         buyer_name : '구매자이름',
@@ -24,13 +24,14 @@ const  Payment = ({cash})=> {
     };
     console.log(IMP);
     console.log(data);
-    /* 4. 결제 창 호출하기 */
+    /* 결제 창 호출하기 */
     IMP.request_pay(data, (response) => {
         
         const { success, error_msg } = response;
    
         if (success) {
           alert('결제 성공');
+          // 2. 결제 후, 해당 사용자의 코인 내역 업데이트 요청 보내기
         } else {
           alert(`결제 실패: ${error_msg}`);
         }
