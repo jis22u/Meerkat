9134ec096f88e401f264087f7aff500be68b70e7
이동훈 <mankind1990s@gmail.com>
2023-02-13 01:19:44 +0900

    feat: be/ order by exitTime desc 적용 #43

--- server/src/main/java/B107/server/meerkat/repository/DealRepository.java
+++ server/src/main/java/B107/server/meerkat/repository/DealRepository.java
@@ -12,12 +12,14 @@ public interface DealRepository extends JpaRepository<Deal, Long> {
     @Query("SELECT new B107.server.meerkat.dto.member.MemPageDealDTO(c.location, c.coin, d.exitTime) " +
             "FROM Call c, Deal d " +
             "WHERE c.idx = d.call.idx " +
-            "AND d.reqMember.idx = :idx")
+            "AND d.reqMember.idx = :idx " +
+            "ORDER BY d.exitTime DESC")
     List<MemPageDealDTO> findAllMyReqHistory(Long idx);
 
     @Query("SELECT new B107.server.meerkat.dto.member.MemPageDealDTO(c.location, c.coin, d.exitTime) " +
             "FROM Call c, Deal d " +
             "WHERE c.idx = d.call.idx " +
-            "AND d.resMember.idx = :idx")
+            "AND d.resMember.idx = :idx " +
+            "ORDER BY d.exitTime DESC")
     List<MemPageDealDTO> findAllMyResHistory(Long idx);
 }
