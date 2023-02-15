3ba598a2986e4583a02d4f6eb0f4fb7b7c971f39
윤승환 <asdryzx@naver.com>
2023-02-15 15:37:49 +0900

    Update client/package.json, client/src/api/auth.js, client/src/api/customAxios.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
-// const BASE_URL = 'https://i8b107.p.ssafy.io/api'
-// const BASE_URL = "http://192.168.31.200:8081/api"
+const BASE_URL = 'https://i8b107.p.ssafy.io/api'
+
 
 export const userLogin = createAsyncThunk(
   'auth/login',
@@ -14,7 +14,7 @@ export const userLogin = createAsyncThunk(
         },
       }
       const { data , headers } = await axios.post(
-        `/login`,
+        `${BASE_URL}/login`,
         form,
         config
       )
@@ -45,7 +45,7 @@ export const registerUser = createAsyncThunk(
         },
       }
       const { data } = await axios.post(
-        `/sign`,  
+        `${BASE_URL}/sign`,  
         form,
         config
       )
