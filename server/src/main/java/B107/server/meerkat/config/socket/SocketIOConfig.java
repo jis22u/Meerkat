080bb19421b5329aaae1cd886d77dae93270d23e
이동훈 <mankind1990s@gmail.com>
2023-02-15 09:49:15 +0900

    fix: chore #54

--- server/src/main/java/B107/server/meerkat/config/socket/SocketIOConfig.java
+++ server/src/main/java/B107/server/meerkat/config/socket/SocketIOConfig.java
@@ -25,7 +25,7 @@ public class SocketIOConfig {
 		com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
 		config.setHostname(host);
 		config.setPort(port);
-		config.setOrigin("https://i8b107.p.ssafy.io");
+		config.setOrigin("https://i8b107.p.ssafy.io/open");
 		return new SocketIOServer(config);
 	}
 
