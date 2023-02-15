ba0d6f8e195b31bdfab900800bee1c1b03d2b438
윤승환 <asdryzx@naver.com>
2023-02-15 15:24:08 +0900

    Update client/src/pages/VideoChat.js

--- client/src/pages/VideoChat.js
+++ client/src/pages/VideoChat.js
@@ -231,7 +231,7 @@ const VideoChat = () => {
       const devices = await navigator.mediaDevices.enumerateDevices();
       cameraOptions.current = devices.filter((device) => device.kind === "videoinput");
 
-      socketRef.current = io("https://i8b107.p.ssafy.io", {
+      socketRef.current = io("http://127.0.0.1:8085", {
         query: `roomName=${roomName}`
       });
     
