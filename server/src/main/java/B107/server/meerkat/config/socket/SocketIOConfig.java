c0d8d45d095014e2da75ec97c76a46bc1b1b358f
이동훈 <mankind1990s@gmail.com>
2023-02-15 14:46:45 +0900

    fix: chore #54

--- server/src/main/java/B107/server/meerkat/config/socket/SocketIOConfig.java
+++ server/src/main/java/B107/server/meerkat/config/socket/SocketIOConfig.java
@@ -25,7 +25,6 @@ public class SocketIOConfig {
 		com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
 //		config.setHostname(host);
 		config.setPort(port);
-		config.setContext("/open/");
 		return new SocketIOServer(config);
 	}
 
