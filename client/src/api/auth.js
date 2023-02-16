ac73e680895d4c50fd63db4dfe485d1d21b94afb
jis22u <jis22u@gmail.com>
2023-02-17 06:46:22 +0900

    merge

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,46 +1,63 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
 
-const BASE_URL = "https://i8b107.p.ssafy.io/api";
+const BASE_URL = 'https://i8b107.p.ssafy.io/api'
 
-export const userLogin = createAsyncThunk("auth/login", async (form, { rejectWithValue }) => {
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const { data, headers } = await axios.post(`${BASE_URL}/login`, form, config);
+export const userLogin = createAsyncThunk(
+  'auth/login',
+  async ( form , { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      const { data, headers } = await axios.post(
+        `${BASE_URL}/login`,
+        form,
+        config
+      );
 
-    localStorage.setItem("userToken", headers.authorization);
+      localStorage.setItem('userToken', headers.authorization)
 
-    return data;
-  } catch (error) {
-    if (error.response && error.response.data.message) {
-      return rejectWithValue(error.response.data.message);
-    } else {
-      return rejectWithValue(error.message);
+      return data
+
+      
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message)
+      } else {
+        return rejectWithValue(error.message)
+      }
     }
   }
-});
+)
 
-export const registerUser = createAsyncThunk("auth/register", async (form, { rejectWithValue }) => {
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const { data } = await axios.post(`${BASE_URL}/sign`, form, config);
+export const registerUser = createAsyncThunk(
+  'auth/register',
+  async ( form, { rejectWithValue }) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      const { data } = await axios.post(
+        `${BASE_URL}/sign`,  
+        form,
+        config
+      )
 
-    return data;
-  } catch (error) {
-    console.log(error);
+      return data
 
-    if (error.response && error.response.data.message) {
-      return rejectWithValue(error.response.data.message);
-    } else {
-      return rejectWithValue(error.message);
+    } catch (error) {
+      console.log(error)
+
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message)
+      } else {
+        return rejectWithValue(error.message)
+      }
     }
   }
-});
+)
--- client/src/store/modules/authSlice.js
+++ client/src/store/modules/authSlice.js
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { registerUser, userLogin } from "api/auth";
+import { createSlice } from '@reduxjs/toolkit'
+import { registerUser, userLogin } from 'api/auth'
+
 
 const initialState = {
   loading: false,
@@ -7,38 +8,38 @@ const initialState = {
   isLogin: false,
   choice: true,
   selected: 0,
-};
+}
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     logout: (state) => {
-      localStorage.removeItem("userToken");
-      state.isLogin = false;
+        localStorage.removeItem('userToken')
+        state.isLogin = false
     },
-    setChoice: (state, { payload }) => {
-      state.choice = payload;
+    setChoice: (state, {payload}) => {
+      state.choice = payload
     },
-    setSelected: (state, { payload }) => {
-      state.selected = payload;
-    },
+    setSelected: (state, {payload}) => {
+      state.selected = payload
+    }
   },
   extraReducers: {
     [userLogin.pending]: (state) => {
       state.loading = true;
     },
-    [userLogin.fulfilled]: (state, { payload: { status } }) => {
-      if (status === "OK") {
+    [userLogin.fulfilled]: (state, { payload : { status }}) => {
+      if (status === 'OK') {
         state.isLogin = true;
       }
       state.loading = false;
     },
     [userLogin.rejected]: (state, { payload }) => {
       state.loading = false;
-      state.error = payload;
+      state.error = payload
     },
-
+    
     [registerUser.pending]: (state) => {
       state.loading = true;
     },
@@ -47,10 +48,11 @@ const authSlice = createSlice({
     },
     [registerUser.rejected]: (state, { payload }) => {
       state.loading = false;
-      state.error = payload;
+      state.error = payload
     },
   },
-});
 
-export const { logout, setChoice, setSelected } = authSlice.actions;
-export default authSlice.reducer;
+})
+
+export const { logout, setChoice, setSelected } = authSlice.actions
+export default authSlice.reducer
