5157745ee4cfde05857362ab704da2503013b30a
윤승환 <asdryzx@naver.com>
2023-02-16 12:23:22 +0900

    Update client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,6 @@ import BigSpinner from 'components/layout/BigSpinner'
 import { onMessageListener } from './api/firebase';
 import { Toast } from 'react-bootstrap';
 import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const Home = lazy(() => import('pages/Home'));
 const VideoChat = lazy(() => import('pages/VideoChat'));
