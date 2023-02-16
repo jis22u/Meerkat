d001a6e04c4ef64a17c788d04d7d7cc334ba54e7
이동훈 <mankind1990s@gmail.com>
2023-02-16 14:46:56 +0900

    fix: be/ findValidMarkes & deleteMarker 수정 #59

--- server/src/main/java/B107/server/meerkat/repository/MarkerRepository.java
+++ server/src/main/java/B107/server/meerkat/repository/MarkerRepository.java
@@ -12,8 +12,9 @@ public interface MarkerRepository extends JpaRepository<Marker, Long> {
 
 	@Query("SELECT m.member.idx " +
 			"FROM Marker m " +
-			"WHERE (m.lat - :lat) * (m.lat - :lat) + (m.lng - :lng) * (m.lng - :lng) <= :range * :range")
-	List<Long> findValidMarkers(@Param("lat") Double lat, @Param("lng") Double lng, @Param("range") Double range);
+			"WHERE (m.lat - :lat) * (m.lat - :lat) + (m.lng - :lng) * (m.lng - :lng) <= :range * :range " +
+			"AND (m.member.idx <> :ownerId OR m.member.idx IS NULL)")
+	List<Long> findValidMarkers(@Param("lat") Double lat, @Param("lng") Double lng, @Param("range") Double range, @Param("idx") Long ownerId);
 
 	@Query("select m from Marker m where m.member.idx = :memberIdx and m.isExp = false")
 	Marker findValidByMemberIdx(@Param("memberIdx") Long memberIdx);
