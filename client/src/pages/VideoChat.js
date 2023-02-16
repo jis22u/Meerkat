6898672af0339fee27e91cac4b6621cb5c7833ad
jis22u <jis22u@gmail.com>
2023-02-16 20:48:50 +0900

    firebase 수정

--- client/src/pages/VideoChat.js
+++ client/src/pages/VideoChat.js
@@ -84,7 +84,6 @@ const VideoChat = () => {
   const [ style, setStyle ] = useState(choice)
   const isJoin = useRef(choice)
   const navigate = useNavigate()
-  const dispatch = useDispatch
 
   const two = useRef(120);
   const five = useRef(300);
@@ -234,6 +233,7 @@ const VideoChat = () => {
       }
       window.onbeforeunload = () => { 
         console.log('새로고침임')
+        if (!choice) roomClose({ roomName, idx })
         return
       }
       await getMedia();
@@ -317,7 +317,6 @@ const VideoChat = () => {
       if (myStream.current) myStream.current.getTracks().forEach(track => track.stop())
       if (!choice) {
         const data = roomClose({ roomName, idx })
-        setChoice(true)
         console.log('폐쇄합니다!')
         dispatch(setChoice(true))
       }
