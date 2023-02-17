7b9ece7572da1915ad923c9734caff853651185f
윤승환 <asdryzx@naver.com>
2023-02-17 09:55:11 +0900

    fix: 필요없는 코드 수정

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -28,8 +28,4 @@ const firebaseConfig = {
     };
   
     self.registration.showNotification(notificationTitle, notificationOptions);
-    self.addEventListener('notificationclick', event => {
-      event.notification.close();
-      event.waitUntil(self.clients.openWindow('https://www.naver.com'))
-    })
   });
